d5d8f8a530f993a1796cc8160653c2159486c6f1
Tim Schraepen <sch3lp@gmail.com>
2021-02-18 14:11:09 +0100

    Updated Unit Test methods with backtick syntax for full sentences

--- src/test/kotlin/be/swsb/coderetreat/HelloTest.kt
+++ src/test/kotlin/be/swsb/coderetreat/HelloTest.kt
@@ -9,18 +9,18 @@ import org.junit.jupiter.api.Test
 class HelloTest {
 
     @Test
-    fun greet_ShouldReturnWorld() {
+    fun `greet - should return World`() {
         assertThat(Hello().greet()).isEqualTo("World!")
     }
 
     @Test
-    fun sayGreeting_WithoutMock_ShouldReturnWorld() {
+    fun `sayGreeting - without mock should return World`() {
         val helloMock = HelloService()
         assertThat(helloMock.sayGreeting()).isEqualTo("World!")
     }
 
     @Test
-    fun sayGreeting_WithMock_ShouldReturnMockedGreeting() {
+    fun `sayGreeting - with mock should return a mocked greeting`() {
         val helloMock = mockk<HelloService>()
         every { helloMock.sayGreeting() } returns "Snarf!"
         assertThat(helloMock.sayGreeting()).isEqualTo("Snarf!")
